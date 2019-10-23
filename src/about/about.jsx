d6874391d5e95ca190999eca9b25fb628f20e33f
Padua <padua.matheus13@gmail.com>
2019-10-23 16:33:59 -0300

    finalizando acoes da lista

---
+++ src/about/about.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader'
+
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='nós' />
+
+        <h3>Nossa História</h3>
+       
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            In eget tempor diam. Duis fringilla, quam nec tempor tincidunt, 
+            enim justo bibendum tortor, sit amet cursus neque erat sit amet 
+            massa. Morbi congue vitae nibh et vehicula. Nam porttitor sapien 
+            sed laoreet pharetra. Proin mollis bibendum est et blandit. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Ut eu nisl 
+            et eros dignissim placerat. Vivamus lobortis dolor in nisi bibendum, 
+            a fermentum purus egestas. Sed tincidunt, lectus sed accumsan commodo, 
+            elit tellus maximus erat, quis sollicitudin dolor leo vitae risus.
+        </p>
+
+        <h3>Missão e Visão</h3>
+        
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            In eget tempor diam. Duis fringilla, quam nec tempor tincidunt, 
+            enim justo bibendum tortor, sit amet cursus neque erat sit amet 
+            massa. Morbi congue vitae nibh et vehicula. Nam porttitor sapien 
+            sed laoreet pharetra. Proin mollis bibendum est et blandit. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Ut eu nisl 
+            et eros dignissim placerat. Vivamus lobortis dolor in nisi bibendum, 
+            a fermentum purus egestas. Sed tincidunt, lectus sed accumsan commodo, 
+            elit tellus maximus erat, quis sollicitudin dolor leo vitae risus.
+            et eros dignissim placerat. Vivamus lobortis dolor in nisi bibendum, 
+            a fermentum purus egestas. Sed tincidunt, lectus sed accumsan commodo, 
+            elit tellus maximus erat, quis sollicitudin dolor leo vitae risus.
+            et eros dignissim placerat. Vivamus lobortis dolor in nisi bibendum, 
+            a fermentum purus egestas. Sed tincidunt, lectus sed accumsan commodo, 
+            elit tellus maximus erat, quis sollicitudin dolor leo vitae risus.
+        </p>
+
+        <h3>Imprensa</h3>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            In eget tempor diam. Duis fringilla, quam nec tempor tincidunt, 
+            enim justo bibendum tortor, sit amet cursus neque erat sit amet 
+            massa. Morbi congue vitae nibh et vehicula. Nam porttitor sapien 
+            sed laoreet pharetra. Proin mollis bibendum est et blandit. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Ut eu nisl 
+            et eros dignissim placerat. Vivamus lobortis dolor in nisi bibendum, 
+            a fermentum purus egestas. Sed tincidunt, lectus sed accumsan commodo, 
+            elit tellus maximus erat, quis sollicitudin dolor leo vitae risus.
+            et eros dignissim placerat. Vivamus lobortis dolor in nisi bibendum, 
+            a fermentum purus egestas. Sed tincidunt, lectus sed accumsan commodo, 
+            elit tellus maximus erat, quis sollicitudin dolor leo vitae risus.
+        </p>
+    
+    </div>
+)
---
+++ src/todo/todoList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import IconButton from '../template/iconButton';
+
+export default props => {
+    
+    const renderRows = () => {
+        const list= props.list || []
+        return list.map(todo =>(
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <IconButton style='success' icon='check' onClick={() => props.handleMarkAsDone(todo)}></IconButton>            
+                    <IconButton style='warning' icon='undo' onClick={() => props.handleMarkAsPending(todo)}></IconButton>            
+                    <IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(todo)}></IconButton>            
+                </td>
+            </tr>
+        ))
+    }
+    
+    return(
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Açōes</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
